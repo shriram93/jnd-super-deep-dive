31db55a52aa9853fa196ced8dc2da3d15d8b4ac9
shriram.ethiraj <prasana.shri@gmail.com>
2021-07-04 23:41:41 +0530

    Test case for adding a new note

--- cloudstorage/src/test/java/com/udacity/jwdnd/course1/cloudstorage/CloudStorageApplicationTests.java
+++ cloudstorage/src/test/java/com/udacity/jwdnd/course1/cloudstorage/CloudStorageApplicationTests.java
@@ -3,11 +3,14 @@ package com.udacity.jwdnd.course1.cloudstorage;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.junit.jupiter.api.*;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.web.server.LocalServerPort;
+
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
@@ -21,6 +24,8 @@ class CloudStorageApplicationTests {
 
 	public String baseURL;
 
+	public WebDriverWait wait;
+
 	@BeforeAll
 	static void beforeAll() {
 		WebDriverManager.chromedriver().setup();
@@ -29,14 +34,15 @@ class CloudStorageApplicationTests {
 	@BeforeEach
 	public void beforeEach() {
 		baseURL = "http://localhost:" + port;
-		this.driver = new ChromeDriver();
+		driver = new ChromeDriver();
+		wait = new WebDriverWait(driver, 5);
 	}
 
 	@AfterEach
 	public void afterEach() {
-//		if (this.driver != null) {
+		if (this.driver != null) {
 //			driver.quit();
-//		}
+		}
 	}
 
 	private void signupAndLogin() {
@@ -56,8 +62,8 @@ class CloudStorageApplicationTests {
 
 	@Test
 	public void loginInvalid() {
-		String username = "testUsername";
-		String password = "testPassword";
+		String username = "user";
+		String password = "password";
 		driver.get(baseURL + "/login");
 
 		LoginPage loginPage = new LoginPage(driver);
@@ -69,17 +75,28 @@ class CloudStorageApplicationTests {
 
 	@Test
 	public void loginSuccess() {
-		signupAndLogin();
+		String firstName = "userFirstName";
+		String lastName = "userLastName";
+		String username = "userUsername";
+		String password = "userPassword";
+		driver.get(baseURL + "/signup");
+
+		SignupPage signupPage = new SignupPage(driver);
+		signupPage.signup(firstName, lastName, username, password);
+		driver.get(baseURL + "/login");
+
+		LoginPage loginPage = new LoginPage(driver);
+		loginPage.login(username, password);
 
 		assertEquals(baseURL + "/home/files", driver.getCurrentUrl());
 	}
 
 	@Test
 	public void signupSuccess() {
-		String firstName = "testFirstName";
-		String lastName = "testLastName";
-		String username = "testUsername";
-		String password = "testPassword";
+		String firstName = "someNewUserFirstName";
+		String lastName = "someNewUserLastName";
+		String username = "someNewUserUsername";
+		String password = "someNewUserPassword";
 		driver.get(baseURL + "/signup");
 
 		SignupPage signupPage = new SignupPage(driver);
@@ -114,4 +131,33 @@ class CloudStorageApplicationTests {
 		assertEquals(baseURL + "/login", driver.getCurrentUrl());
 	}
 
+	@Test
+	public void homeNavbarShouldRouteProperly() {
+		signupAndLogin();
+
+		HomePage homePage = new HomePage(driver);
+		assertEquals(baseURL + "/home/files", driver.getCurrentUrl());
+
+		homePage.getNavbarNotesTab().click();
+		assertEquals(baseURL + "/home/notes", driver.getCurrentUrl());
+
+		homePage.getNavbarCredentialsTab().click();
+		assertEquals(baseURL + "/home/credentials", driver.getCurrentUrl());
+
+		homePage.getNavbarFilesTab().click();
+		assertEquals(baseURL + "/home/files", driver.getCurrentUrl());
+	}
+
+	@Test
+	public void notesAddNoteSuccess() {
+		signupAndLogin();
+		HomePage homePage = new HomePage(driver);
+		homePage.getNavbarNotesTab().click();
+		homePage.getAddNoteBtn().click();
+		wait.until(ExpectedConditions.visibilityOf(homePage.getNoteTitleInput()));
+		homePage.addNote("note title", "note description");
+		wait.until(ExpectedConditions.visibilityOf(homePage.getNotesTable()));
+		List<WebElement> notes = homePage.getNotes();
+		assertEquals(1, notes.size());
+	}
 }
--- cloudstorage/src/test/java/com/udacity/jwdnd/course1/cloudstorage/HomePage.java
+++ cloudstorage/src/test/java/com/udacity/jwdnd/course1/cloudstorage/HomePage.java
@@ -1,9 +1,12 @@
 package com.udacity.jwdnd.course1.cloudstorage;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
+
+import java.util.List;
 
 public class HomePage {
 
@@ -16,8 +19,79 @@ public class HomePage {
     @FindBy(css="#upload-btn")
     private WebElement uploadBtn;
 
+    @FindBy(css="#nav-files-tab")
+    private WebElement navbarFilesTab;
+
+    @FindBy(css="#nav-notes-tab")
+    private WebElement navbarNotesTab;
+
+    @FindBy(css="#nav-credentials-tab")
+    private WebElement navbarCredentialsTab;
+
+    @FindBy(css="#add-note-btn")
+    private WebElement addNoteBtn;
+
+    @FindBy(css="#add-credential-btn")
+    private WebElement addCredentialBtn;
+
+    @FindBy(css="#note-title")
+    private WebElement noteTitleInput;
+
+    @FindBy(css="#note-description")
+    private WebElement noteDescriptionInput;
+
+    @FindBy(css="#note-submit-btn")
+    private WebElement noteSubmitBtn;
+
+    @FindBy(css="#notes-table")
+    private WebElement notesTable;
+
+    @FindBy(css=".note")
+    private List<WebElement> notes;
+
+    @FindBy(css="#credential-url")
+    private WebElement credentialUrlInput;
+
+    @FindBy(css="#credential-username")
+    private WebElement credentialUsernameInput;
+
+    @FindBy(css="#credential-password")
+    private WebElement credentialPasswordInput;
+
+    @FindBy(css="#credential-submit-btn")
+    private WebElement credentialSubmitBtn;
+
+    @FindBy(css="#credentials-table")
+    private WebElement crdentialsTable;
+
     public HomePage(WebDriver webDriver) {
         PageFactory.initElements(webDriver, this);
+    }
+
+    public void addNote(String title, String description) {
+        noteTitleInput.sendKeys(title);
+        noteDescriptionInput.sendKeys(description);
+        noteSubmitBtn.click();
+    }
+
+    public List<WebElement> getNotes() {
+        return notes;
+    }
+
+    public WebElement getNoteEditBtn(WebElement note) {
+        return note.findElement(By.className("note-edit-btn"));
+    }
+
+    public WebElement getNoteDeleteBtn(WebElement note) {
+        return note.findElement(By.className("note-delete-btn"));
+    }
+
+    public WebElement getNoteTitle(WebElement note) {
+        return note.findElement(By.className("note-title"));
+    }
+
+    public WebElement getNoteDescription(WebElement note) {
+        return note.findElement(By.className("note-description"));
     }
 
     public WebElement getLogOutBtn() {
@@ -30,5 +104,37 @@ public class HomePage {
 
     public WebElement getFileUploadBtn() {
         return fileUploadBtn;
+    }
+
+    public WebElement getNavbarFilesTab() {
+        return navbarFilesTab;
+    }
+
+    public WebElement getNavbarNotesTab() {
+        return navbarNotesTab;
+    }
+
+    public WebElement getNavbarCredentialsTab() {
+        return navbarCredentialsTab;
+    }
+
+    public WebElement getAddNoteBtn() {
+        return addNoteBtn;
+    }
+
+    public WebElement getAddCredentialBtn() {
+        return addCredentialBtn;
+    }
+
+    public WebElement getNoteTitleInput() {
+         return noteTitleInput;
+    }
+
+    public WebElement getNotesTable() {
+        return notesTable;
+    }
+
+    public WebElement getCrdentialsTable() {
+        return crdentialsTable;
     }
 }
